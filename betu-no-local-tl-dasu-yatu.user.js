b745f8001d1f43bd24deea835387fff57b584f1d
adcin2k17 <LUL@example.com>
2017-04-19 15:39:30 +0900

    modified:   betu-no-local-tl-dasu-yatu.user.js いい感じに頑張ったで@versionをあげた

--- betu-no-local-tl-dasu-yatu.user.js
+++ betu-no-local-tl-dasu-yatu.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         betu no local tl nagasu yatu
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://mstdn.maud.io/*
