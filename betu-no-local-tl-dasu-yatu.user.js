1fcd6a1b045ff34626a94992f4d0db4c50265289
adcin2k17 <LUL@example.com>
2017-04-19 16:05:30 +0900

    modified:   betu-no-local-tl-dasu-yatu.user.js フォローするとき(リモートフォローするときではない)に何かが巻き起こってフォローのボタンが反応せずフォローできないのを直した @versionをあげた

--- betu-no-local-tl-dasu-yatu.user.js
+++ betu-no-local-tl-dasu-yatu.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         betu no local tl nagasu yatu
 // @namespace    http://tampermonkey.net/
-// @version      0.2.1
+// @version      0.2.2
 // @description  try to take over the world!
 // @author       You
-// @match        https://mstdn.maud.io/*
+// @match        https://mstdn.maud.io/web/*
 // @grant        none
 // ==/UserScript==
 
